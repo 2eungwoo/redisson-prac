83d4d00832915205aede6a9798603c2780ca56c2
swlee <swo98@naver.com>
2025-08-20 13:43:24 +0900

    dependency: spock 프레임워크 사용을 위한 의존성 추가

---
+++ src/test/groovy/study/redissonprac/service/TicketServiceSpec.groovy
@@ -0,0 +1,19 @@
+import spock.lang.Specification
+
+class TicketServiceSpec extends Specification {
+
+    def "setup"() {
+    }
+
+    def "teardown"() {
+    }
+
+    def "test"() {
+        given:
+
+        when:
+
+        then:
+
+    }
+}
