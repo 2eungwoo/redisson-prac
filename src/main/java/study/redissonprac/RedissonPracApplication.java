a19113ec39b378ef087a435e91a1ac09a338ec21
swlee <swo98@naver.com>
2025-08-20 11:08:34 +0900

    init: init project

---
+++ src/main/java/study/redissonprac/RedissonPracApplication.java
@@ -0,0 +1,13 @@
+package study.redissonprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissonPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonPracApplication.class, args);
+    }
+
+}
